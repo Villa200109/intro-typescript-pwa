f218d73dae7ba3762b662d7ddaefd3f81a6ce6e9
Villa200109 <villalobos_123000@hotmail.com>
2022-09-13 20:45:46 -0500

    tercer commit

---
+++ src/ejemplos-ts/01-tipo-basico.ts
@@ -0,0 +1,25 @@
+/* variables */
+
+var num1 =2;
+let num2 =1;
+const num3=2;
+
+/* function sumar(){
+    var tem=3;
+    console.log(tem);
+    tem=45;
+}
+
+sumar(); */
+
+
+let nombre:string='Roberto';
+let num:number=12;
+let activo:boolean=true;
+
+let matricula:number|string|boolean = "342jh";
+matricula=123123;
+matricula=false;
+
+
+// nombre=23;
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-
-
-console.log('Hola Mundo! IEVN1001');
-
-/*
-    ===== Código de TypeScript =====
-*/
-
